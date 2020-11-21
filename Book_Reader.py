cdfbcd2d210e1d329b4286351e4f22a6fe7bc704
TheITQC <31858454+TheITQC-QA@users.noreply.github.com>
2020-11-21 19:47:47 +0530

    Update Book_Reader.py

--- Book_Reader.py
+++ Book_Reader.py
@@ -5,7 +5,7 @@ path = "E://PythonStuff//pywinauto.pdf"   #You can add your file path/location h
 reader_init = pyttsx3.init()
 file=open(path,"rb")
 reader=PyPDF2.PdfFileReader(file)
-pages = reader.numPages
+#pages = reader.numPages
 page1=reader.getPage(8)
 pdfData=page1.extractText()
 reader_init.say(pdfData)
